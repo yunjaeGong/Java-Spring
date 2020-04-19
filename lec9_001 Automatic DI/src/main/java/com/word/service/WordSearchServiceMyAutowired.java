94f0ef78f94d9bb56f3063c4f31d77e41b09add6
YunjaeGong <llegokid@naver.com>
2020-04-19 23:38:15 +0900

    .

--- lec9_001 Automatic DI/src/main/java/com/word/service/WordSearchServiceMyAutowired.java
+++ lec9_001 Automatic DI/src/main/java/com/word/service/WordSearchServiceMyAutowired.java
@@ -3,34 +3,34 @@ package com.word.service;
 import com.word.WordSet;
 import com.word.dao.WordDao;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.annotation.Resource;
 
-public class WordRegisterServiceMyAutowired {
+public class WordSearchServiceMyAutowired {
 	@Resource
 	private WordDao wordDao;
+	// @Autowired
 
-	public WordRegisterServiceMyAutowired() {
+	public WordSearchServiceMyAutowired() {
 
 	}
 
-	// @Autowired
-	public WordRegisterServiceMyAutowired(WordDao wordDao) {
+	public WordSearchServiceMyAutowired(WordDao wordDao) {
 		this.wordDao = wordDao;
 	}
 	
-	public void register(WordSet wordSet) {
-		String wordKey = wordSet.getWordKey();
+	public WordSet searchWord(String wordKey) {
 		if(verify(wordKey)) {
-			wordDao.insert(wordSet);
+			return wordDao.select(wordKey);
 		} else {
-			System.out.println("The word has already been registered.");
+			System.out.println("WordKey information is available.");
 		}
+		
+		return null;
 	}
 	
 	public boolean verify(String wordKey){
 		WordSet wordSet = wordDao.select(wordKey);
-		return wordSet == null ? true : false;
+		return wordSet != null ? true : false;
 	}
 	
 	public void setWordDao(WordDao wordDao) {
